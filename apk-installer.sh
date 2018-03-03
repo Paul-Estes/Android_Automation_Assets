068be6435b6f433c3fad8145bca6ff05aecec081
Cody Estes <cody@vantageled.com>
2018-03-03 13:36:22 -0800

    Local Update

--- apk-installer.sh
+++ apk-installer.sh
@@ -1,32 +1,8 @@
-echo "---- ANDROID INSTALLATION AND SETUP ----"
-echo "Preparing to launch..."
-timeout  3s
-echo "---- ANDROID INSTALLATION AND SETUP ----"
-echo "Turning on hardware timer"
-adb shell busybox hwclock --systohc
-timeout  2s 
-echo "---- ANDROID INSTALLATION AND SETUP ----"
-echo "Installing Terminal"
-adb install terminal.apk
-echo "---- ANDROID INSTALLATION AND SETUP ----"
-echo "Installing TeamViewer"
+#!/usr/bin/fish
+adb shell "busybox hwclock --systohc"
 adb install TeamViewerHost12.apk
-echo "---- ANDROID INSTALLATION AND SETUP ----"
-echo "Installing TeamViewer-Add-On"
 adb install base.apk
-echo "---- ANDROID INSTALLATION AND SETUP ----"
-echo "Installing Watchdog"
 adb install smwatchdog_1_0_4.apk
-echo "---- ANDROID INSTALLATION AND SETUP ----"
-echo "Installing Bridge"
-adb install smbridge_1_1_0.apk
-echo "---- ANDROID INSTALLATION AND SETUP ----"
-echo "Installing SM Player"
-adb install smplayer_1_0_3.apk
-echo "---- ANDROID INSTALLATION AND SETUP ----"
-echo "Android Setup and Installation Complete!"
-echo "Starting SM Suite..."
-adb shell am startservice com.sminfinity.smwatchdog/.MainService
-timeout  5s
-echo "---- ANDROID INSTALLATION AND SETUP ----"
-echo "Batch Complete!"
+adb install smbridge_1_2_1.apk
+adb install smplayer_1_0_4.apk
+adb shell "am startservice com.sminfinity.smwatchdog/.MainService"
