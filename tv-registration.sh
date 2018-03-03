a7d827e2ddff8d1d5f052c4795cc25b917ceee69
Cody Estes <cody@vantageled.com>
2018-03-03 13:33:00 -0800

    Added TeamViewer Registration Fish Script

---
+++ tv-registration.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/fish
+adb shell input text "remote@sminfinity.com"
+adb shell input keyevent KEYCODE 61
+adb shell input text "T3am5MASH!c1ub"
+adb shell input keyevent KEYCODE 61
+adb shell input keyevent KEYCODE 66
