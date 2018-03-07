dadae780b3f40cdb32634d9dac9a8727d67e9770
Cody Estes <cody@vantageled.com>
2018-03-07 08:04:54 -0800

    reworked inital automation from Fernando. Added project reference variable

--- apk-installer.sh
+++ apk-installer.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/fish
+read --prompt "echo 'Project Ref #: ' " -l ref
 adb shell "busybox hwclock --systohc"
 adb install TeamViewerHost12.apk
 adb install base.apk
@@ -6,3 +7,34 @@ adb install smwatchdog_1_0_4.apk
 adb install smbridge_1_2_1.apk
 adb install smplayer_1_0_4.apk
 adb shell "am startservice com.sminfinity.smwatchdog/.MainService"
+#Begin SM Bridge initialization
+#Sets Project reference from $ref (Line2) and default credentials
+adb shell sleep 60
+adb shell am start -a android.intent.action.VIEW -d http://localhost:8080
+adb shell input keyevent KEYCODE 61
+adb shell input text $ref
+adb shell input keyevent KEYCODE 61
+adb shell input text "admin"
+adb shell input keyevent KEYCODE 61
+adb shell input text "VANtageLED123"
+adb shell input keyevent KEYCODE 61
+adb shell input text "user"
+adb shell input keyevent KEYCODE 61
+adb shell input text "user"
+adb shell input keyevent KEYCODE 61
+adb shell input text "readonly"
+adb shell input keyevent KEYCODE 61
+adb shell input text "readonly"
+adb shell input keyevent KEYCODE 61
+adb shell input keyevent KEYCODE 66
+adb shell input tap 945 615
+adb shell input text "admin"
+adb shell input keyevent KEYCODE 61
+adb shell input text "VANtageLED123"
+adb shell input keyevent KEYCODE 61
+adb shell input keyevent KEYCODE 66
+#Begin TeamViewer initalization & registration
+#Need to note future clicks for further automation
+adb shell sleep 45
+adb shell am start com.teamviewer.host.market/com.teamviewer.host.ui.HostActivity
+adb shell am start -a android.intent.action.VIEW -d https://get.teamviewer.com/sm-android
